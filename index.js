6a721f670a9cc24851c4ccacf62d8c8792800339
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-05-15 14:41:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//For every card if clicked, show the content
+
+let playingCard = document.getElementsByClassName("card");
+
+/*
+function hidingCard(){
+    let hideCard = playingCard.style.display = "none";
+
+    
+
+
+}
+
+if (card){
+    setTimeout(hideCard, 2000);
+}else{
+    return 
+}
+*/
+
+/*
+let cards = [1,2,3,4,6];
+for (let i=0; i<5; i +=1){
+    console.log(cards[i]);
+}
+*/
+
+
+
+//function shuffleCards() {
+    let cards = [1, 2, 3, 4, 5];
+    for (let i = 0; i < cards.length; i++) {
+        
+        console.log(cards[i]);
+    }
+    //return cards[i];
+//}
+
+
+
+//console.log(shuffleCards);
